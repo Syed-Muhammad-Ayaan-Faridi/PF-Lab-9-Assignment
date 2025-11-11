c7a0d6a186d6a907d9a3c63c796e2f0965ffa02d
Syed Muhammad Ayaan Faridi <k253103@nu.edu.pk>
2025-11-11 11:54:31 +0500

    Add files via upload

---
+++ Q2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+void studentStanding(int array[], int upperbound)
+{
+    int marks,Total = 0;
+    float avgMarks=0.0;
+    printf("Enter your 3 subject marks between 0 to 100\n");
+    for (int i = 0; i<(upperbound-1); i++)
+    {
+        printf("Enter your marks of subject %d: ", (i + 1));
+        scanf("%d", &array[i]);
+        while ((array[i] < 0) || (array[i] > 100))
+        {
+            printf("Re-enter your marks of subject %d between 0-100: ", (i + 1));
+            scanf("%d", &array[i]);
+        }
+        Total += array[i];
+    }
+    avgMarks = (float)Total/(upperbound-1);
+    printf("Average marks: %.2f\n",avgMarks);
+    //Checking fail codition wrt Marks
+    for (int i = 0; i < (upperbound-1); i++)
+    {
+        if (array[i] <= 40)
+        {
+            printf("Fail\n");
+            printf("Reason: Failed asyu have less than 40 marks in subject %d\n",(i+1));
+            return;
+        }
+    }
+    //Checking failed condition wrt Average marks
+    if (avgMarks > 50.0)
+    {
+        printf("Passed\n");
+    }
+    else
+    {
+        printf("FAILED\nReason: Failed as your average marks is less than 50\n");
+        return;
+    }
+}
+
+int main()
+{
+    int marks[4], mark;
+    studentStanding(marks,4);
+    return 0;
+}
